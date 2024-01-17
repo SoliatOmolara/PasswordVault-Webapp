2c6bac8ffcf77349b6d68bff4ceb2c27051f04af
SoliatOmolara <azeezsoleehah@gmail.com>
2024-01-18 00:19:36 +0100

    Added the Index page

--- PasswordVault_Project/PasswordVault_App/models.py
+++ PasswordVault_Project/PasswordVault_App/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class PasswordVault(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    website_name = models.CharField(max_length=250)
+    website_link = models.URLField()
+    date = models.DateTimeField()
+    
+    def __str__(self):
+        return self.website_name
--- PasswordVault_Project/PasswordVault_Project/settings.py
+++ PasswordVault_Project/PasswordVault_Project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'PasswordVault_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
